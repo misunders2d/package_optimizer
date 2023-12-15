5d9b786ab1097a587682b5f1409d54e75fd256bc
MISUNDERSTOOD <2djohar@gmail.com>
2023-12-15 17:44:45 +0200

    sides of products sorted from low to max

--- utils.py
+++ utils.py
@@ -274,18 +274,17 @@ def read_prepare_file(file_obj, limit = 0.5, limit2 = 0.5, mode = 'lengths', top
     file = file.dropna(subset = ['side1', 'side2', 'side3', 'weight, lbs'])
     products = file.values.tolist()
     
-    columns = ['Product', 'option','side1', 'side2', 'side3', 'weight, lbs', 'size tier', 'storage fee','fulfillment fee']
+    columns = ['Product', 'option','side1', 'side2', 'side3', 'weight, lbs', 'size tier', 'storage fee','fulfillment fee', 'total fee']
     
     df = pd.DataFrame(columns = columns)
     for c in products:
         c[1:4] = sorted(c[1:4])
         item = Box(c[1], c[2], c[3], c[4])
-        fees = item.storage_fees
-        current_fee = fees['combined']
         temp_product = pd.DataFrame([c], columns = ['Product', 'side1', 'side2', 'side3', 'weight, lbs'])
         temp_product['size tier'] = item.size_tier
-        temp_product['storage fee'] = current_fee
+        temp_product['storage fee'] = item.storage_fees['combined']
         temp_product['fulfillment fee'] = item.fulfillment_fees['combined']
+        temp_product['total fee'] = item.total_fee
         temp_product['option'] = 'original'
         temp_product = temp_product[columns]
 
@@ -293,11 +292,12 @@ def read_prepare_file(file_obj, limit = 0.5, limit2 = 0.5, mode = 'lengths', top
         temp_option = pd.DataFrame(columns = columns)
         for i, option in enumerate(best_options):
             temp = pd.DataFrame(
-                [[c[0]]+ [f' option {i+1}']+list(option.shape) + [option.weight, option.size_tier, option.storage_fees['combined'], option.fulfillment_fees['combined']]],
+                [[c[0]]+ [f' option {i+1}']+list(option.shape) + [option.weight, option.size_tier, option.storage_fees['combined'], option.fulfillment_fees['combined'],option.total_fee]],
                 columns = columns)
+            temp['savings,$'] = temp['total fee'] - item.total_fee
             temp_option = pd.concat([temp_option, temp])
         df = pd.concat([df, temp_product, temp_option])
-        df['total fee'] = df['storage fee'] + df['fulfillment fee']
+        
     buf = BytesIO()
     with pd.ExcelWriter(buf, engine = 'xlsxwriter') as writer:
         df.to_excel(writer, index = False)
